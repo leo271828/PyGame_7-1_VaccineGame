afc983d9ad227dc3fd7bf011b5e987b982536f55
jingjiehaung <47046706+jingjiehaung@users.noreply.github.com>
2021-08-10 22:26:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ class Main:
         self.stuff.update()
         # 升級機制
 
-    def gameover(self, screen, tops=[]):
+    def gameover(self, screen):
         '''結束畫面'''
         # monster還未完成所以未加進來
         screen.blit(background_image,(0,0)) #增
@@ -57,21 +57,16 @@ class Main:
         self.monster.repaint(screen, position)
         self.player.repaint(screen, position)
         self.stuff.repaint(screen, position)
-        f = pygame.font.Font('data/freesansbold.ttf', 90)
-        text1 = f.render('Game Over', True, [255,255,100])
-        text2 = f.render('Score: %s' % self.player.score, True, self.player.color)
+        f = pygame.font.Font('data/freesansbold.ttf', 70)
+        f2 = pygame.font.Font('data/freesansbold.ttf', 50)
+        text1 = f.render('Game Over', True, [255, 255, 0])
+        text2 = f2.render('Vaccine coverage: {} %' .format(self.player.sumvaccine), True, self.player.color)
         rect1 = text1.get_rect()
         rect2 = text2.get_rect()
-        rect1.center = [wh[0]/2, wh[1]/2 - 300]
-        rect2.center = [wh[0]/2, wh[1]/2 - 150]
+        rect1.center = [wh[0] / 2, wh[1] / 2 - 150]
+        rect2.center = [wh[0] / 2, wh[1] / 2 - 50]
         screen.blit(text1, rect1)
         screen.blit(text2, rect2)
-
-        f = pygame.font.Font('data/freesansbold.ttf', 50)
-        for i in range(len(tops)):
-            color = self.player.color if int(tops[i]) == self.player.score else [255,255,255]
-            text = f.render('%s: %s' % (i+1, tops[i]), True, color)
-            screen.blit(text, (wh[0]/2-100, wh[1]/2 + 50*(i-1)))
 
         pygame.display.flip()
         pygame.display.update()
@@ -90,7 +85,7 @@ class Main:
         play = True     #設定遊戲狀態
         done = False
         timeup = False
-        timelimit = 10  # sec
+        timelimit = 180  # sec
         t0 = time.time()
         self.reset()
         pygame.mixer.init()
@@ -106,11 +101,9 @@ class Main:
                 # 這裡原本有 e.type == pygame.KEYDOWN 那一塊 我全部刪掉了 所以這行你可以槓掉
             # 判定存活與否
             if not play:
-                self.gameover(self.screen, tops)
+                self.gameover(self.screen)
             elif self.player.blood <= 0 or timeup:
                 play = False
-                #音檔
-                tops = self.top(self.player.score)
                 #音檔
             else:
                 self.update()
@@ -140,26 +133,6 @@ class Main:
         #print(minute, second)
         self.screen.blit(text_time, ((time_rect_size - f.get_linesize() * 1.38) / 2, wh[1] - f.get_height()))
         pygame.display.update()
-    
-    # 其實也不用算排行 下面一坨也可以刪掉
-    @staticmethod
-    def top(score):
-        '''計算排行榜，排序分數，返回前七名並儲存到文件裡'''
-        try:
-            with open('data/score.txt', 'r') as file:
-                data = file.read()
-                score_list = data.split('\n')
-        except FileNotFoundError:
-            score_list = ['0' for i in range(7)]
-        for s in range(len(score_list)):
-            if score > int(score_list[s]):
-                score_list.insert(s, str(score))
-                break
-        while len(score_list) > 7:
-            del score_list[-1]
-        with open('data/score.txt', 'w') as file:
-            file.write('\n'.join(score_list))
-        return score_list
 
 root = Main()
 root.begin()
